ff127636cced5690f071ecc2c7baa8d1a58a7ed6
Stilsi <angelo_nuque@dlsu.edu.ph>
2023-10-22 23:55:07 +0800

    Updated location-page

---
+++ location-page.js
@@ -0,0 +1,56 @@
+window.addEventListener("scroll", function () {
+    var navbar = document.querySelector(".navbar");
+    var scrollY = window.scrollY;
+    var navLinks = document.querySelectorAll(".navbar .nav-item a");
+    var profileButtons = document.querySelectorAll(".dropbtn");
+    var profileDropdown = document.getElementById("profile-dropdown"); // Select the profile dropdown by its ID
+    var profileDropdownContent = profileDropdown.querySelector(".dropdown-content"); // Select the dropdown content within the profile dropdown
+    var dropdownContent = document.querySelector(".dropdown-content");
+    
+    if (scrollY > 100) {
+      navbar.style.backgroundColor = "#263a29";
+      navbar.style.transition = "background-color 0.3s";
+      navLinks.forEach(function (link) {
+        link.style.color = "#f2e3db";
+        link.style.transition = "color 0.3s";
+      });
+  
+      profileButtons.forEach(function (button) {
+        button.style.color = "#f2e3db";
+      });
+  
+      dropdownContent.style.backgroundColor = "#263a29";
+      profileDropdownContent.style.backgroundColor = "#263a29"; // Change the background color of the specific dropdown content
+    } else {
+      navbar.style.backgroundColor = "transparent";
+      navLinks.forEach(function (link) {
+        link.style.color = "#263a29";
+      });
+  
+      profileButtons.forEach(function (button) {
+        button.style.color = "#263a29";
+      });
+  
+      dropdownContent.style.backgroundColor = "#f1f1f1";
+      profileDropdownContent.style.backgroundColor = "#f1f1f1"; // Reset the background color
+    }
+  });
+  
+
+function sortCards(sortType) {
+    // Get all sorting buttons
+    const buttons = document.querySelectorAll('.sort');
+  
+    // Loop through all buttons and remove the 'active' class
+    buttons.forEach((button) => {
+      button.classList.remove('active');
+    });
+  
+    // Add the 'active' class to the clicked button
+    document.getElementById(`sort-${sortType}`).classList.add('active');
+  
+    // Now you can implement your sorting logic here based on the 'sortType'
+    // For example, you can sort the cards based on the selected criteria.
+    // Implement your sorting logic here.
+  }
+  
