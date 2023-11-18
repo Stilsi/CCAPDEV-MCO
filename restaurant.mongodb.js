83aa5227aeb0cc6679cf705187a99ed3a47e38d9
daniellecorral <145423687+daniellecorral@users.noreply.github.com>
2023-11-18 19:06:01 +0800

    Update restaurant.mongodb.js

--- restaurant.mongodb.js
+++ restaurant.mongodb.js
@@ -3,15 +3,15 @@ use('restaurantDB');
 
 // Insert a few documents into the sales collection.
 db.getCollection('restaurants').insertMany([
-    {  _id: "ashdiasd",
-        name: "asdasd",
+    {  _id: "dominos_pizza",
+        name: "Domino's Pizza",
         logo: {
             type: String,
-            default: '/public/assets/restaurant-icons/atericas.png'   
+            default: '/public/assets/restaurant-icons/dominos.png'   
             },
-        location: "asds",
-        socialMedia: "asds",
-        phoneNumber: "asds",
+        location: "E.A. Fernandez Bldg, G/F, 2510 Taft Ave, Manila, 1004 Metro Manila",
+        socialMedia: "customercare@dominospizza.ph",
+        phoneNumber: "(02) 8997 3030",
         recommendedCount: 0,
         notRecommendedCount: 0, 
     },
