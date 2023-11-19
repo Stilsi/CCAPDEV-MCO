f81e1c6f361ac152d52fbf6c92398636bdf85cf5
ashoolotl <layne_vitan@dlsu.edu.ph>
2023-11-20 03:46:32 +0800

    adding reviews now working

--- controller/restaurant.js
+++ controller/restaurant.js
@@ -1,28 +1,32 @@
 import express from "express";
 import Restaurant from "../model/restaurantschema.js";
+import Review from '../model/reviewschema.js';
 
 const router = express.Router();
 
 router.get('/:id', async (req, res) => {
-  try {
-   const restaurantId = req.params.id;
-   const restaurant = await Restaurant.findById(restaurantId);
-  
-   if (!restaurant) {
-     console.error(`No restaurant found with ID: ${restaurantId}`);
-     return res.status(404).send('Restaurant not found');
-   }
-  
-   console.log('Restaurant data:', restaurant);
-   res.render("restaurant", { pageTitle: restaurant.name, restaurant });
+ try {
+ const restaurantId = req.params.id;
+ const restaurant = await Restaurant.findById(restaurantId);
+ const reviews = await Review.find({ restaurant: restaurantId });
  
-   req.session.restaurant = restaurant;
-   console.log('Session ID:', req.sessionID); 
-   console.log('Current Restaurant:', req.session.restaurant.name); 
-  } catch (error) {
-   console.error(`Error fetching restaurant data: ${error.message}`);
-   res.status(500).send('Internal Server Error');
-  }
- }); 
+ if (!restaurant) {
+  console.error('No restaurant found with ID: ${restaurantId}');
+  return res.status(404).send('Restaurant not found');
+ }
+ 
+ console.log('Restaurant data:', restaurant);
+ res.render("restaurant", { pageTitle: restaurant.name, restaurant, reviews });
+
+ req.session.restaurant = restaurant;
+ req.session.restaurantId = restaurant._id; // Set the restaurant ID in the session
+ console.log('Session ID:', req.sessionID); 
+ console.log('Current Restaurant:', req.session.restaurant.name); 
+ } catch (error) {
+ console.error('Error fetching restaurant data: ${error.message}');
+ res.status(500).send('Internal Server Error');
+ }
+});
+
 
 export default router;
