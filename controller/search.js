81a522782c9353e3f0f8a5b4393a48c6ee8d37cc
Stilsi <angelo_nuque@dlsu.edu.ph>
2023-11-20 03:47:46 +0800

    Updated Search features
    BACKEND IS SO :skull:

--- controller/search.js
+++ controller/search.js
@@ -1,16 +1,25 @@
 import express from "express";
+import Restaurant from "../model/restaurantschema.js";
 
 const router = express.Router();
 
 router.get('/', function(req, res) {
-    res.render('search');
+ res.render('search');
 });
 
 router.get('/search', async function(req, res) {
     const query = req.query.query;
-    const restaurants = await Restaurant.find({});
-    const results = restaurants.filter(restaurant => restaurant.name.includes(query));
-    res.render('search', {results: results});
-});
+    if (!query) {
+      return res.status(400).send('No query provided');
+    }
+    try {
+      const restaurants = await Restaurant.find({ $name: { $search: query } });
+      console.log('restaurants data:', restaurants); 
+      res.render('search', {results: restaurants});
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('An error occurred while performing the search');
+    }
+  });
 
 export default router;
--- public/js/nav-bar-search.js
+++ public/js/nav-bar-search.js
@@ -1,8 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("search-form").addEventListener("submit", function(e) {
+  document.getElementById("search-form").addEventListener("submit", function(e) {
       e.preventDefault();
-      const searchBox = document.getElementById("search-box").value;
-      window.location.href = `search-page.html?query=${searchBox}`;
-    });
+      const searchBox = document.getElementById("search-box");
+      const searchQuery = searchBox.value;
+ 
+      // Validate the search query
+      if (!searchQuery || typeof searchQuery !== 'string') {
+          alert('Please enter a valid search query');
+          return;
+      }
+ 
+      // Redirect to the search results page with the search query as a query parameter
+      window.location.href = `/search?query=${encodeURIComponent(searchQuery)}`;
   });
-
+ });
+ 
