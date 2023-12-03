79f44b84bf0fcfb5e4043773692078a621296077
Stilsi <angelo_nuque@dlsu.edu.ph>
2023-12-04 01:53:28 +0800

    "trying" to make the sorting work
    WTFHASJHDAKJSHDHJKHKJAHKSDJHKASDHJKASDJKHAHJKSD

--- controller/search.js
+++ controller/search.js
@@ -19,4 +19,23 @@ router.get('/', async(req, res) => {
  }
 });
 
+router.get('/sort', async(req, res) => {
+  const sortType = req.query.type;
+  let sortQuery;
+  if (sortType === 'alphabetically') {
+     sortQuery = { name: 1 };
+  } else if (sortType === 'by-rating') {
+     sortQuery = { rating: -1 };
+  } else {
+     return res.status(400).send('Invalid sort type');
+  }
+  try {
+     const restaurants = await Restaurant.find().sort(sortQuery);
+     res.render('search', {results: restaurants});
+  } catch (error) {
+     console.error(error);
+     res.status(500).send('An error occurred while performing the sort');
+  }
+});
+
 export default router;
--- controller/sortCard.js
+++ controller/sortCard.js
@@ -1,17 +1,46 @@
-function sortCards(sortType) {
-    let results = document.querySelector('.container').children;
-    if (sortType === 'alphabetically') {
-     Array.from(results).sort((a, b) => {
-       return a.querySelector('.name').innerText.localeCompare(b.querySelector('.name').innerText);
-     }).forEach(el => {
-       el.parentNode.appendChild(el);
-     });
-    } else if (sortType === 'by-rating') {
-     Array.from(results).sort((a, b) => {
-       return b.querySelector('.rating').innerText - a.querySelector('.rating').innerText;
-     }).forEach(el => {
-       el.parentNode.appendChild(el);
-     });
-    }
-   }
-   
+import fetch from 'node-fetch';
+
+async function sortCards(type) {
+  // Fetch the sorted data from the server
+  const response = await fetch(`/sort?type=${type}`);
+  const sortedData = await response.json();
+
+  // Get the container where the cards will be appended
+  const container = document.querySelector(".container");
+
+  // Create a document fragment to hold the new cards
+  const fragment = document.createDocumentFragment();
+
+  // Create a new card for each item in the sorted data
+  sortedData.forEach(item => {
+    // Create the card element
+    const card = document.createElement("div");
+    card.className = "card";
+
+    // Add the item's data to the card
+    const logo = document.createElement("img");
+    logo.className = "card-logo";
+    logo.src = item.logo;
+    logo.alt = `${item.name} Logo`;
+    card.appendChild(logo);
+
+    const title = document.createElement("div");
+    title.className = "card-title";
+    title.textContent = item.name;
+    card.appendChild(title);
+
+    // Add the card to the document fragment
+    fragment.appendChild(card);
+  });
+
+  // Remove all existing cards from the container
+  while (container.firstChild) {
+    container.firstChild.remove();
+  }
+
+  // Append the new cards to the container
+  container.appendChild(fragment);
+}
+
+// Export the function so it can be imported and used in other files
+export { sortCards };
