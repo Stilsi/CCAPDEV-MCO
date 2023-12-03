65eaa362066e1432b22161f244d72cdf3ac3babe
ashoolotl <layne_vitan@dlsu.edu.ph>
2023-12-04 01:38:29 +0800

    "remember me" now working

--- controller/login.js
+++ controller/login.js
@@ -1,27 +1,35 @@
+// login.js
 import express from "express";
 import User from "../model/userschema.js";
 
 const router = express.Router();
 
 router.get('/', function(req, res) {
-    const warningMessage = req.query.warning; // Get warning message from query parameters
+    const warningMessage = req.query.warning;
     res.render('login', { warningMessage });
 });
 
 router.post('/', async (req, res) => {
-    const { username, password } = req.body;
+    const { username, password, remember } = req.body;
 
     const user = await User.findOne({ username });
 
     if (!user) {
-        return res.redirect('/login?warning=Username not found'); // Pass warning message in query parameters
+        return res.redirect('/login?warning=Username not found');
     }
 
     if (user.password !== password) {
-        return res.redirect('/login?warning=Invalid password'); // Pass warning message in query parameters
+        return res.redirect('/login?warning=Invalid password');
     }
 
     req.session.user = user;
+
+    if (remember) {
+        req.session.cookie.maxAge = 3 * 7 * 24 * 60 * 60 * 1000;
+    } else {
+        req.session.cookie.maxAge = null;
+    }
+
     console.log('Session ID:', req.sessionID);
     console.log('Session User:', req.session.user.username);
     res.redirect('/homepage');
--- controller/logout.js
+++ controller/logout.js
@@ -1,18 +1,19 @@
+// logout.js
 import express from 'express';
 
 const router = express.Router();
 
 router.get('/', function(req, res) {
-req.session.destroy(function(err) {
-if(err) {
-    console.log(err);
-} else {
-    console.log('Session ID:', req.sessionID); 
-    console.log('Session destroyed'); // Add this line
-    res.redirect('/login');
-}
+    req.session.cookie.maxAge = null; // Clear the "remember" setting
+    req.session.destroy(function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Session ID:', req.sessionID);
+            console.log('Session destroyed');
+            res.redirect('/login');
+        }
+    });
 });
-});
-   
 
 export default router;
